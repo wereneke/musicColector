c56565ab3159acc767254d81fe0472155a92cd23
wera dabrowska <weronikad752@gmail.com>
2017-04-17 16:01:53 +0200

    tuple-list maker works

--- musicColector.py
+++ musicColector.py
@@ -1,6 +1,19 @@
-def make_music_list(list_of_csv_lines):
-    print(list_of_csv_lines)
+def list_of_list_of_lines(list_of_csv_lines):
+    lista = [line.split(' | ') for line in list_of_csv_lines]
+    for i in range(len(lista)):
+        lista[i][4] = lista[i][4].strip('\n')
+    return lista
 
+
+def tupling(lista):
+    music = []
+    for i in range(len(lista)):
+        name = [lista[i][j] for j in range(len(lista[i])) if j < 2]
+        information = [lista[i][j] for j in range(len(lista[i])) if j >= 2]
+        music += [tuple([tuple(name), tuple(information)])]
+    return music
+
+        
 menu = '''Welcome in the CoolMusic! Choose the action:
          1) Add new album
          2) Find albums by artist
@@ -8,13 +21,17 @@ menu = '''Welcome in the CoolMusic! Choose the action:
          4) Find musician by album
          5) Find albums by letter(s)
          6) Find albums by genre
-         7) Calculate the age of all albums 
+         7) Calculate the age of all albums
          8) Choose a random album by genre
          9) Show the amount of albums by an artist *
         10) Find the longest-time album *
          0) Exit'''
-music = []
+
 
 csv_file = open('music.txt')
 list_of_csv_lines = csv_file.readlines()
 csv_file.close()
+lista = list_of_list_of_lines(list_of_csv_lines)
+
+music = tupling(lista)
+print(music)
